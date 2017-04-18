99acfddab4e406e5c6d1d7d3191c52ac27d97d43
hiepph <hoanghiepjp96@gmail.com>
2017-04-18 08:12:42 +0700

    fix(framgia): Killed when bot cell now is '-2'

--- framgia_train.py
+++ framgia_train.py
@@ -194,7 +194,7 @@ class Board():
                 return KILL_POINT
             else:
                 return FATAL_POINT
-        elif value == '4':
+        elif value == '-2':
             return KILLED_POINT
         else:
             return (bot.score - bot.last_score) * BOOST_POINT
