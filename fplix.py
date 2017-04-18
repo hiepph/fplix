7a40b91ec2af99b9e2ed0735705cbfd07261e742
Nguyen Thien Dzung <dungtoanhoc@yahoo.com.vn>
2017-04-18 13:43:03 +0700

    feat(): add new bot

--- fplix.py
+++ fplix.py
@@ -44,10 +44,28 @@ class Board():
         for i in range(H):
             self.state[i] = map(int, list(inputs.readline()[:-1]))
 
+        self.botInfo = []
+        for i in range(0, 4):
+            self.botInfo.append({
+                "id": i * 2 + 1,
+                "stable": [],
+                "unstable": [],
+            })
+
+        for i in range(H):
+            for j in range(W):
+                square = self.state[i][j]
+                if square == 0: continue
+                botId = (square - 1) / 2
+                if square % 2 == 0: 
+                    self.botInfo[botId]["unstable"].append((i, j))
+                else:
+                    self.botInfo[botId]["stable"].append((i, j))
+
     def getPosition(self, x, y):
         return self.state[x][y]
 
-    def mahattanDistance(self, pos1, pos2):
+    def manhattanDistance(self, pos1, pos2):
         return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
     def setBots(self, bots):
@@ -65,18 +83,14 @@ class Bot():
 
     # state: Board' state format a.k.a 2-d array
     def distFromEnemies(self):
-        unstable = []
-        for x in range(0, H):
-            for y in range(0, W):
-                if self.board.getPosition(x, y) == self.id + 1:
-                    unstable.append((x, y))
+        unstable = self.board.botInfo[self.id / 2]['unstable']
 
         squares = unstable + [(self.x, self.y)]
         distances = []
         for bot in self.board.bots:
             if bot.id == self.id: continue
             if bot.x == -1 or bot.y == -1: continue
-            dist = [self.board.mahattanDistance((bot.x, bot.y), (x, y)) for (x, y) in squares]
+            dist = [self.board.manhattanDistance((bot.x, bot.y), (x, y)) for (x, y) in squares]
             distances.append(min(dist))
 
         return distances
@@ -99,16 +113,36 @@ class Bot():
         for bot in self.board.bots:
             if bot.id == self.id: continue
             if abs(bot.x - newX) <= 1 and abs(bot.y - newY) <= 1:
-                return False
+                if self.board.getPosition(newX, newY) != self.id: return False
+
 
         return True
 
-    def shortestDistToHome(self, square, prev=None):
-        # BFS
+    def shortestDistToSquare(self, square): 
         visited = [[False for y in range(0, W)] for x in range(0, H)]
 
         x, y = square
-        queue = [(x, y, prev, 0, [])]   
+        queue = [(self.x, self.y, self.prev, 0, [])]   
+
+        while len(queue) > 0:
+            curX, curY, curPrev, length, history = queue.pop(0)
+            visited[curX][curY] = True
+
+            if (curX, curY) == square:
+                return length, history
+            actions = self.possibleActions((curX, curY), prev=curPrev)
+            for action in actions:
+                nextX, nextY = NEXT_ACTIONS[action]
+                newX, newY = curX + nextX, curY + nextY
+                if not visited[newX][newY]:
+                    queue.append((newX, newY, action, length + 1, history + [action]))
+        return -1, []
+
+    def shortestDistToHome(self):
+        # BFS
+        visited = [[False for y in range(0, W)] for x in range(0, H)]
+
+        queue = [(self.x, self.y, self.prev, 0, [])]   
 
         while len(queue) > 0:
             curX, curY, curPrev, length, history = queue.pop(0)
@@ -130,7 +164,7 @@ class Bot():
         queue = [(self.x, self.y, self.prev, 0, [])]
         while len(queue) > 0:
             curX, curY, curPrev, length, history = queue.pop(0)
-            visited[curX][curY]
+            visited[curX][curY] = True
             if self.board.getPosition(curX, curY) != self.id:
                 return length, history
             actions = self.possibleActions((curX, curY), prev=curPrev)
@@ -142,13 +176,13 @@ class Bot():
         return -1, []
 
     def chooseAction(self):
-        # self.prev = 'LEFT'
+        self.prev = 'LEFT'
         distFromEnemies = self.distFromEnemies()
         minDistToEnemy = min(distFromEnemies)
 
-        length, history = self.shortestDistToHome((self.x, self.y), prev=self.prev)
+        length, history = self.shortestDistToHome()
         actions = self.possibleActions((self.x, self.y), self.prev)
-        
+
         if len(actions) == 0:
             move = 'LEFT'
             self.prev = move
@@ -171,18 +205,57 @@ class Bot():
             self.prev = move
             return move
 
-        if length >= minDistToEnemy - 3:
+        if minDistToEnemy - length <= 3:
             move = history[0]
             self.goBack = True
             self.prev = move
             return move
 
-        if length < minDistToEnemy - 3:
-            for action in actions:
-                if action != history[0]:
-                    move = action
-                    break
+        if minDistToEnemy - length > 3:
+            for bot in self.board.bots:
+                if bot.id == self.id: continue
+                dist = [self.board.manhattanDistance((self.x, self.y), square) for square in self.board.botInfo[bot.id / 2]['unstable']]
+                if len(dist) == 0: continue
+                minDist = min(dist)
+                square = self.board.botInfo[bot.id / 2]['unstable'][dist.index(minDist)]
+                toHome, _ = bot.shortestDistToHome()
+                if toHome > minDist:
+                    length, hist = self.shortestDistToSquare(square)
+                    if len(hist) == 0: continue
+                    if hist[0] in actions:
+                        move = hist[0]
+                        self.prev = move
+                        return move
 
+            option = []
+            for action in actions:
+                meetUnstable = False
+                step = NEXT_ACTIONS[action]
+                nextX, nextY = self.x + step[0], self.y + step[1]
+                count = 1
+                while nextX >= 0 and nextX < H and nextY >= 0 and nextY < W:
+                    if self.board.getPosition(nextX, nextY) == self.id + 1:
+                        meetUnstable = True
+                        break
+                    nextX, nextY = nextX + step[0], nextY + step[1]
+                    count += 1
+                option.append((meetUnstable, count))
+
+            selected = None
+            maxCount = -1
+            for i, action in enumerate(actions):
+                meetUnstable, count = option[i]
+                if meetUnstable: continue
+                if count > maxCount:
+                    selected = action
+                    maxCount = count
+
+            if selected is not None:
+                move = selected
+                self.prev = move
+                return move
+
+            move = history[0]
             self.prev = move
             return move
 
