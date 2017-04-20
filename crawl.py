29a544668efaf1516c4a43ad09c64b82830b99a9
hiepph <hoanghiepjp96@gmail.com>
2017-04-21 01:39:26 +0700

    perf: Crawl more games

--- crawl.py
+++ crawl.py
@@ -4,7 +4,7 @@ import time
 # https://aicontest.framgia.vn/game/4843/replay.rpl
 url = 'https://aicontest.framgia.vn/game'
 cookie = 'session=.eJxNj8FqwkAURX-lvHUW7cRSDLgIxGAL8zQyVvKKBI2TTGecBibRtCP592rooou7PefcKxSVk62CqHNnGUDxeYToCg8HiIAna03J6cRF7Emv1W0G9WuIetOTJZPrMiQ779HzGQwBlM1R9ntXdI2RXyOlg-gDQpZOLT7J72nGF5NameaSZWwVz20tdAXBnw2G3Z3RuuofYMy4qZG9W2RkyG5-luJN8aR8zLXSfJta9KSWwnguakZb7pHlzxjPxqRzK914CSYvMPwCCtFMtA.C9Zjqw.OFVrQWaaXZesiur418fGDgIDCzM'
-for i in range(2290, 13000):
+for i in range(2290, 15000):
   gameUrl = url + '/' + str(i) + '/replay.rpl'
   print 'Game ' + str(i)
   try:
